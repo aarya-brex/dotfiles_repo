f997db82d56ba6ce20a8dcf63eb67d8bd87c1029
Anubhaw Arya <aarya@brex.com>
2023-11-30 11:00:45 -0800

    some tooling

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 echo Y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+kubectl krew install ctx
+kubectl krew install ns
+echo "alias kprod='kubectl ctx production && kubectl ns production'" >> ~/.zshrc
+echo "alias kstage='kubectl ctx staging && kubectl ns staging'" >> ~/.zshrc
+echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> ~/.zshrc
